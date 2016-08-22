5f326f59477ad12b2d036cdaabbe6483da5117d1
Aljona Murygina <murygina@synyx.de>
2016-08-22 16:14:37 +0200

    Make AbstractLicensePlateHandlerTest abstract
    * refs #16271

--- src/test/java/net/contargo/types/AbstractLicensePlateHandlerTest.java
+++ src/test/java/net/contargo/types/AbstractLicensePlateHandlerTest.java
@@ -11,7 +11,7 @@ import java.util.function.Consumer;
  *
  * @author  Aljona Murygina - murygina@synyx.de
  */
-class AbstractLicensePlateHandlerTest {
+abstract class AbstractLicensePlateHandlerTest {
 
     LicensePlateHandler handler;
 
