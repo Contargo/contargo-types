cb019ad243927a998be33ba57e0d7991d2c1c280
Aljona Murygina <murygina@synyx.de>
2016-08-25 15:16:39 +0200

    Add container loading state type
    * refs #16271

---
+++ src/main/java/net/contargo/types/container/LoadingState.java
@@ -0,0 +1,13 @@
+package net.contargo.types.container;
+
+/**
+ * Describes the loading state of a {@link net.contargo.domain.Container}.
+ *
+ * @author  Aljona Murygina - murygina@synyx.de
+ * @since  0.2.0
+ */
+public enum LoadingState {
+
+    FULL,
+    EMPTY
+}
