829000623269ac8f06a3ab5b8573d5e000344819
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2018-03-08 10:56:37 +0100

    add custom din5008 compliant phone number formatting

--- src/main/java/net/contargo/types/telephony/formatting/PhoneNumberFormatter.java
+++ src/main/java/net/contargo/types/telephony/formatting/PhoneNumberFormatter.java
@@ -38,8 +38,47 @@ public class PhoneNumberFormatter {
     }
 
 
-    public String parseAndFormatToDIN5008(final String phoneNumber) {
+    public String parseAndFormatToDIN5008(final String phoneNumber) throws PhoneNumberFormattingException {
 
-        throw new UnsupportedOperationException("not implemented yet");
+        final String phoneNumberWithoutWhitespace = phoneNumber.replaceAll("\\s+", "");
+
+        final Phonenumber.PhoneNumber parsedNumber;
+
+        parsedNumber = parsePhoneNumber(phoneNumber, phoneNumberWithoutWhitespace);
+
+        final String formattedNumber = phoneNumberUtil.format(parsedNumber,
+                PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+
+        final String nationalNumber = String.valueOf(parsedNumber.getNationalNumber());
+        final int countryCode = parsedNumber.getCountryCode();
+        final PhoneNumberUtil.PhoneNumberType phoneNumberType = PhoneNumberUtil.getInstance()
+                .getNumberType(parsedNumber);
+
+        return formatDIN5008(countryCode, nationalNumber, formattedNumber,
+                phoneNumberType.equals(PhoneNumberUtil.PhoneNumberType.FIXED_LINE));
+    }
+
+
+    private String formatDIN5008(final int countryCode, final String nationalNumber, final String preFormattedNumber,
+        final boolean isFixedLine) {
+
+        final String numberFormat = "+%d %s %s";
+
+        final String areaCode;
+        final String connectionNumber;
+        final String chunkedConnectionNumber;
+
+        if (isFixedLine) { // if the number is a fixed number take the area code from the preformatted number
+
+            areaCode = preFormattedNumber.split(" ")[1];
+            connectionNumber = preFormattedNumber.split(" ")[2].replaceAll("\\D", "");
+        } else {
+            areaCode = nationalNumber.substring(0, 3); // take the first three digits
+            connectionNumber = nationalNumber.substring(3, nationalNumber.length()).replaceAll("\\D", "");
+        }
+
+        chunkedConnectionNumber = String.join(" ", connectionNumber.split("(?<=\\G.{4})"));
+
+        return String.format(numberFormat, countryCode, areaCode, chunkedConnectionNumber);
     }
 }
--- src/test/java/net/contargo/types/telephony/formatting/PhoneNumberFormatterTest.java
+++ src/test/java/net/contargo/types/telephony/formatting/PhoneNumberFormatterTest.java
@@ -3,10 +3,20 @@ package net.contargo.types.telephony.formatting;
 import com.google.i18n.phonenumbers.NumberParseException;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class PhoneNumberFormatterTest {
+
+    private PhoneNumberFormatter phoneNumberFormatter;
+
+    @Before
+    public void init() {
+
+        phoneNumberFormatter = new PhoneNumberFormatter();
+    }
+
 
     @Test
     public void ensureThatE164FormatPhoneNumberFromInputStringFormatsValidNumber()
@@ -22,5 +32,50 @@ public class PhoneNumberFormatterTest {
 
         final PhoneNumberFormatter phoneNumberFormatter = new PhoneNumberFormatter();
         phoneNumberFormatter.parseAndFormatToE164Format("i321%6&_-?`");
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsRussianMobile() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+7 922 5551 234", phoneNumberFormatter.parseAndFormatToDIN5008("+7 (922) 555-1234"));
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsLongGermanMobileNumber() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+49 171 1234 5678 9123",
+            phoneNumberFormatter.parseAndFormatToDIN5008("0171123456789123"));
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsGermanFixedNumberWithoutCountryCode() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+49 6222 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("06222123456"));
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsGermanFixedNumberWithCountryCode() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+49 6222 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("+496222123456"));
+        Assert.assertEquals("+49 6222 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("00496222123456"));
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsGermanMobileNumberWithCountryCode() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+49 171 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("+49171123456"));
+        Assert.assertEquals("+49 171 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("0049171123456"));
+    }
+
+
+    @Test
+    public void ensureThatDIN5008FormatsGermanMobileNumberWithSlash() throws PhoneNumberFormattingException {
+
+        Assert.assertEquals("+49 171 1234 56", phoneNumberFormatter.parseAndFormatToDIN5008("0171/123456"));
     }
 }
